bd20ff4a394e58475d3059bfbf9d53c5541256dc
GiangMH <hoanggiangdh10th@gmail.com>
2017-06-02 17:58:02 +0700

    GiangMH add

---
+++ LearnWPFMVVM/ManageStudent/Utils/Service.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infragistics.Windows.DataPresenter;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ManageStudent.Utils
+{
+    public class Service
+    {
+       public DataGrid  InitDataGrid(String colNames, DataGrid dgr)
+        {
+            char[] delimiterChars = { '|' };
+            String[] m_List = colNames.Split(delimiterChars);
+            foreach (var colName in m_List)
+            {
+                DataGridTextColumn textColumn = new DataGridTextColumn();
+                textColumn.Header = colName;
+                textColumn.Binding = new Binding(colName);
+                dgr.Columns.Add(textColumn);
+            }
+            return dgr;
+         }
+    }
+}
