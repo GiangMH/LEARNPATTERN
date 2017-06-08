534bc4c2201e4d06a5bd3f1c215abec410cb6a74
GiangMH <hoanggiangdh10th@gmail.com>
2017-06-08 17:59:16 +0700

    GiangMH thêm class mới

---
+++ LearnWPFMVVM/ManageStudent/ViewModel/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ManageStudent.ViewModel
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void PropertyChangedEvent(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                PropertyChanged(this, e);
+            }
+        }
+    }
+}
---
+++ LearnWPFMVVM/ManageStudent/ViewModel/ViewModelStudent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using ManageStudent.Model;
+
+namespace ManageStudent.ViewModel
+{
+    class ViewModelStudent:BaseViewModel
+    {
+        private IList<ModelStudent> m_StudentList;
+        private DataSet m_StudentDS;
+
+        public IList<ModelStudent> STUDENTLS
+        {
+            get
+            {
+                return m_StudentList;
+            }
+            set
+            {
+                this.m_StudentList = value;
+                base.PropertyChangedEvent("STUDENTLS");
+            }
+        }
+        public DataSet STUDENTDS
+        {
+            get
+            {
+                return m_StudentDS;
+            }
+            set
+            {
+                m_StudentDS = value;
+                base.PropertyChangedEvent("STUDENTDS");
+            }
+        }
+    }
+}
